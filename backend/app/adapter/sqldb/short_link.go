619ea325c430eab8f317627456932397d326a257
Marcello Paiva <marcello.paiva@gmail.com>
2021-02-18 10:24:35 -0500

    preparing for merge

--- backend/app/adapter/sqldb/short_link.go
+++ backend/app/adapter/sqldb/short_link.go
@@ -157,11 +157,13 @@ WHERE "%s"=$5;`,
 // GetShortLinkByAlias finds the active ShortLink in short_link table with a given alias.
 func (s ShortLinkSQL) GetShortLinkByAlias(alias string, expiringAt time.Time) (entity.ShortLink, error) {
 	statement := fmt.Sprintf(`
-SELECT "%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"
+SELECT "%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"
 FROM "%s" 
 WHERE "%s"=$1 AND "%s">$2;`,
 		table.ShortLink.ColumnAlias,
 		table.ShortLink.ColumnLongLink,
+		table.ShortLink.ColumnRoom,
+		table.ShortLink.ColumnID,
 		table.ShortLink.ColumnExpireAt,
 		table.ShortLink.ColumnCreatedAt,
 		table.ShortLink.ColumnUpdatedAt,
@@ -184,6 +186,8 @@ WHERE "%s"=$1 AND "%s">$2;`,
 	err := rows.Scan(
 		&shortLink.Alias,
 		&shortLink.LongLink,
+		&shortLink.Room,
+		&shortLink.ID,
 		&shortLink.ExpireAt,
 		&shortLink.CreatedAt,
 		&shortLink.UpdatedAt,
